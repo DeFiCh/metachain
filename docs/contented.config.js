703cc75b78f42487f9efc9ca1648af29c94f4eb1
Fuxing Loh <4266087+fuxingloh@users.noreply.github.com>
2022-08-03 13:49:04 +0800

    chore(docs/deps): bump contented to v1.1.0 (#21)

---
+++ docs/contented.config.js
@@ -0,0 +1,44 @@
+const defaultFields = {
+  title: {
+    type: 'string',
+  },
+  description: {
+    type: 'string',
+  }
+};
+
+/** @type {import('@birthdayresearch/contented').ContentedConfig} */
+module.exports = {
+  preview: {
+    name: "DeFi Meta Chain",
+    url: "https://defimetachain.org",
+    github: {
+      url: "https://github.com/defich/metachain"
+    }
+  },
+  processor: {
+    rootDir: '../',
+    pipelines: [
+      {
+        type: 'Docs',
+        pattern: ['./README.md', './docs/**/*.md'],
+        processor: 'md',
+        fields: defaultFields,
+        transform: (file) => {
+          if (file.path === '/readme') {
+            file.fields.title = 'DeFi Meta Chain';
+            file.path = '/';
+            file.sections = [];
+          } else {
+            file.path = file.path.replaceAll(/^\/docs\/?/g, '/');
+            file.sections = file.sections.slice(1);
+          }
+          return file;
+        },
+        sort: (a, b) => {
+          return a.path === '/' ? -1 : 0;
+        },
+      }
+    ],
+  },
+};
