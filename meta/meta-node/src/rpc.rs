a020309252c30010433a12b26f9704999c40237f
canonbrother <w.canonbrother@gmail.com>
2022-08-04 14:49:50 +0800

    add guide on module.merge

--- meta/meta-node/src/rpc.rs
+++ meta/meta-node/src/rpc.rs
@@ -48,6 +48,11 @@ where
 
 	module.merge(System::new(client.clone(), pool.clone(), deny_unsafe).into_rpc())?;
 	module.merge(TransactionPayment::new(client.clone()).into_rpc())?;
+	
+	// Extend this RPC with a custom API by using the following syntax.
+	// `YourRpcStruct` should have a reference to a client, which is needed
+	// to call into the runtime.
+	// `module.merge(YourRpcTrait::into_rpc(YourRpcStruct::new(ReferenceToClient, ...)))?;`
 	module.merge(ManualSealApiServer::into_rpc(ManualSeal::new(command_sink)))?;
 
 	Ok(module)
