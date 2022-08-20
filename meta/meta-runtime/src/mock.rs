ae3421715d517a2d0525414841bf0d7d3d9327f4
canonbrother <w.canonbrother@gmail.com>
2022-08-20 16:02:44 +0800

    chore(meta): add evm (#27)
    * cherry pick 3be6c1f
    
    * restructure
    
    * fix merge
    
    * fix merge 2
    
    * rebuild cargo.lock
    
    * fix TransactionByteFee
    
    * missing event
    
    * rm comment
    
    * add guide on module.merge
    
    * clippy fix
    
    * fix lint
    
    * rm unuse dep and ver on cargo.toml
    
    * deps alignments
    
    * use deps polkadot-v0.9.25 to support pallet-evm
    
    * wip evm - 2 errs
    
    * ordering
    
    * added evm
    
    * use double quotes on Cargo.toml
    
    * re-organize deps
    
    * add evm config on chain_spec
    
    * evm base fee, fixed gas weight mapping, tx converter
    
    * node service
    
    * command.rs
    
    * chain_spec.rs
    
    * rm cmd sink on service.rs
    
    * update dep on node cargo
    
    * cli
    
    * refine service
    
    * add btreemap in rpc
    
    * rm cfg feature manual-seal
    
    * phew fix all
    
    * add mock and test
    
    * minimize unncessary changes
    
    minimize unnecessary chg chain_spec.rs
    
    minimize unnecessary chg cmd.rs
    
    minimize unnecessary chg rpc.rs
    
    * rm log
    
    * chainId 988
    
    * fix conflicts
    
    * fmt
    
    * missing default sealing
    
    * bump(deps-dev): bump @birthdayresearch/contented from 1.1.0 to 1.1.1 in /docs (#29)
    
    bump(deps-dev): bump @birthdayresearch/contented in /docs
    
    Bumps @birthdayresearch/contented from 1.1.0 to 1.1.1.
    
    ---
    updated-dependencies:
    - dependency-name: "@birthdayresearch/contented"
      dependency-type: direct:development
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * ss58prefix 988
    
    * add DFI token symbol
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

---
+++ meta/meta-runtime/src/mock.rs
@@ -0,0 +1,154 @@
+#![cfg(test)]
+use super::*;
+
+use core::str::FromStr;
+use fp_evm::GenesisAccount;
+use frame_support::{
+	construct_runtime, parameter_types,
+	traits::{ConstU64, Everything, GenesisBuild},
+};
+use pallet_evm::{EnsureAddressNever, EnsureAddressRoot, IdentityAddressMapping};
+use sp_core::H160;
+use sp_runtime::{
+	testing::Header,
+	traits::{BlakeTwo256, IdentityLookup},
+};
+use std::collections::BTreeMap;
+
+pub type AccountId = H160;
+pub type Balance = u64;
+
+type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
+type Block = frame_system::mocking::MockBlock<Test>;
+
+pub const INITIAL_BALANCE: Balance = 1_000_000_000_000_000;
+
+pub fn alice() -> H160 {
+	H160::from_str("1000000000000000000000000000000000000001").unwrap()
+}
+
+construct_runtime!(
+		pub enum Test where
+			Block = Block,
+			NodeBlock = Block,
+			UncheckedExtrinsic = UncheckedExtrinsic,
+		{
+			System: frame_system,
+			Balances: pallet_balances,
+			Timestamp: pallet_timestamp,
+			EVM: pallet_evm,
+		}
+);
+
+parameter_types! {
+	pub const BlockHashCount: u64 = 250;
+}
+
+impl frame_system::Config for Test {
+	type BaseCallFilter = Everything;
+	type BlockWeights = ();
+	type BlockLength = ();
+	type Origin = Origin;
+	type Call = Call;
+	type Index = u64;
+	type BlockNumber = u64;
+	type Hash = H256;
+	type Hashing = BlakeTwo256;
+	type AccountId = AccountId;
+	type Lookup = IdentityLookup<Self::AccountId>;
+	type Header = Header;
+	type Event = Event;
+	type BlockHashCount = BlockHashCount;
+	type DbWeight = ();
+	type Version = ();
+	type PalletInfo = PalletInfo;
+	type AccountData = pallet_balances::AccountData<u64>;
+	type OnNewAccount = ();
+	type OnKilledAccount = ();
+	type SystemWeightInfo = ();
+	type SS58Prefix = ();
+	type OnSetCode = ();
+	type MaxConsumers = frame_support::traits::ConstU32<16>;
+}
+
+parameter_types! {
+	pub const ExistentialDeposit: u64 = 1;
+}
+impl pallet_balances::Config for Test {
+	type Balance = Balance;
+	type DustRemoval = ();
+	type Event = Event;
+	type ExistentialDeposit = ExistentialDeposit;
+	type AccountStore = System;
+	type WeightInfo = ();
+	type MaxLocks = ();
+	type MaxReserves = ();
+	type ReserveIdentifier = [u8; 8];
+}
+
+impl pallet_timestamp::Config for Test {
+	type Moment = u64;
+	type OnTimestampSet = ();
+	type MinimumPeriod = ConstU64<1>;
+	type WeightInfo = ();
+}
+
+impl pallet_evm::Config for Test {
+	type FeeCalculator = ();
+	type GasWeightMapping = ();
+	type CallOrigin = EnsureAddressRoot<AccountId>;
+	type WithdrawOrigin = EnsureAddressNever<AccountId>;
+	type AddressMapping = IdentityAddressMapping;
+	type Currency = Balances;
+	type Event = Event;
+	type Runner = pallet_evm::runner::stack::Runner<Self>;
+	type PrecompilesType = ();
+	type PrecompilesValue = ();
+	type ChainId = ();
+	type OnChargeTransaction = ();
+	type BlockGasLimit = BlockGasLimit;
+	type BlockHashMapping = pallet_evm::SubstrateBlockHashMapping<Self>;
+	type FindAuthor = ();
+}
+
+pub(crate) struct ExtBuilder {
+	// Accounts endowed with balances
+	balances: Vec<(AccountId, Balance)>,
+}
+
+impl Default for ExtBuilder {
+	fn default() -> Self {
+		ExtBuilder { balances: vec![] }
+	}
+}
+
+impl ExtBuilder {
+	pub(crate) fn build(self) -> sp_io::TestExternalities {
+		let mut t = frame_system::GenesisConfig::default()
+			.build_storage::<Test>()
+			.expect("Test ExtBuilder setup successfully");
+		pallet_balances::GenesisConfig::<Test> {
+			balances: vec![(H160::default(), INITIAL_BALANCE)],
+		}
+		.assimilate_storage(&mut t)
+		.expect("Pallet balances storage can be assimilated");
+		let mut accounts = BTreeMap::<H160, GenesisAccount>::new();
+		accounts.insert(
+			alice(),
+			GenesisAccount {
+				nonce: U256::from("1"),
+				balance: U256::from(INITIAL_BALANCE),
+				storage: Default::default(),
+				code: vec![
+					0x00, // STOP
+				],
+			},
+		);
+		GenesisBuild::<Test>::assimilate_storage(&pallet_evm::GenesisConfig { accounts }, &mut t)
+			.unwrap();
+
+		let mut ext = sp_io::TestExternalities::new(t);
+		ext.execute_with(|| System::set_block_number(1));
+		ext
+	}
+}
