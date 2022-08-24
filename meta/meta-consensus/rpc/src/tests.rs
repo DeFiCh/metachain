7df137b60e005bdc1b82f161f148f0d7f563b4f2
surangap <surangatco@gmail.com>
2022-08-25 01:33:59 +0800

    Update get_block_hash() test

--- meta/meta-consensus/rpc/src/tests.rs
+++ meta/meta-consensus/rpc/src/tests.rs
@@ -3,12 +3,14 @@ use super::*;
 use substrate_test_runtime_client::{
     AccountKeyring::*, DefaultTestClientBuilderExt, TestClientBuilder, TestClientBuilderExt,
 };
+use sp_consensus::{SelectChain};
 
-#[test]
-fn simpleTest1() {
+#[tokio::test]
+async fn get_block_hash() {
     let builder = TestClientBuilder::new();
     let (client, select_chain) = builder.build_with_longest_chain();
     let client = Arc::new(client);
 
-    MetaConsensusRpc::new(client).into_rpc();
+    // check if genesis block hash is returned correctly.
+    assert_eq!(MetaConsensusRpc::new(client.clone()).get_block_hash(0.into()).unwrap().to_string(), select_chain.leaves().await.unwrap()[0].to_string());
 }
