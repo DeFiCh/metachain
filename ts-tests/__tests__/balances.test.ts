e7b9301a09b486d159649da25802301533428019
canonbrother <w.canonbrother@gmail.com>
2022-08-24 23:16:03 +0800

    update bal.test.ts

--- ts-tests/__tests__/balances.test.ts
+++ ts-tests/__tests__/balances.test.ts
@@ -1,6 +1,13 @@
-import { MetaDContainer, genesis } from '../containers';
+import {
+  GENESIS_ACCOUNT,
+  GENESIS_ACCOUNT_BALANCE,
+  GENESIS_ACCOUNT_PRIVATE_KEY,
+  EXISTENTIAL_DEPOSIT,
+  MetaDContainer
+} from '../containers';
 
 const container = new MetaDContainer();
+const TEST_ACCOUNT = '0x1111111111111111111111111111111111111111';
 
 beforeAll(async () => {
   await container.start();
@@ -10,7 +17,46 @@ afterAll(async () => {
   await container.stop();
 });
 
-it('should get balance', async () => {
-  const bal = await container.web3?.eth.getBalance(genesis.address);
-  expect(bal).toStrictEqual('340282366920938463463374607431768210955');
+it('should genesis balance setup correctly', async () => {
+  const bal = await container.web3?.eth.getBalance(GENESIS_ACCOUNT);
+  expect(bal).toStrictEqual(GENESIS_ACCOUNT_BALANCE);
+});
+
+it('should transfer balance', async () => {
+  const value = '0x200'; // 512, must be higher than ExistentialDeposit
+  const gasPrice = '0x3B9ACA00'; // 1000000000
+  const tx = await container.web3?.eth.accounts.signTransaction(
+    {
+      from: GENESIS_ACCOUNT,
+      to: TEST_ACCOUNT,
+      value: value,
+      gasPrice: gasPrice,
+      gas: '0x100000'
+    },
+    GENESIS_ACCOUNT_PRIVATE_KEY
+  );
+  await container.call('eth_sendRawTransaction', [tx?.rawTransaction]);
+
+  // GENESIS_ACCOUNT_BALANCE - (21000 * gasPrice) - value;
+  const expectedGenesisBalance = (
+    BigInt(GENESIS_ACCOUNT_BALANCE) -
+    BigInt(21000) * BigInt(gasPrice) -
+    BigInt(value)
+  ).toString();
+  const expectedTestBalance = (Number(value) - EXISTENTIAL_DEPOSIT).toString();
+  expect(
+    await container.web3?.eth.getBalance(GENESIS_ACCOUNT, 'pending')
+  ).toStrictEqual(expectedGenesisBalance);
+  expect(
+    await container.web3?.eth.getBalance(TEST_ACCOUNT, 'pending')
+  ).toStrictEqual(expectedTestBalance);
+
+  await container.generate();
+
+  expect(await container.web3?.eth.getBalance(GENESIS_ACCOUNT)).toStrictEqual(
+    expectedGenesisBalance
+  );
+  expect(await container.web3?.eth.getBalance(TEST_ACCOUNT)).toStrictEqual(
+    expectedTestBalance
+  );
 });
--- ts-tests/containers/MetaDContainer.ts
+++ ts-tests/containers/MetaDContainer.ts
@@ -12,11 +12,15 @@ import { Keyring } from '@polkadot/api';
 const keyringEth = new Keyring({ type: 'ethereum' });
 
 // constant
-export const CHAIN_ID = 988;
 export const GENESIS_ACCOUNT = '0x6be02d1d3665660d22ff9624b7be0551ee1ac91b';
+export const GENESIS_ACCOUNT_BALANCE =
+  '340282366920938463463374607431768210955';
 export const GENESIS_ACCOUNT_PRIVATE_KEY =
   '0x99B3C12287537E38C90A9219D4CB074A89A16E9CDB20BF85728EBD97C343E342';
 export const genesis = keyringEth.addFromUri(GENESIS_ACCOUNT_PRIVATE_KEY);
+
+export const CHAIN_ID = 988;
+export const EXISTENTIAL_DEPOSIT = 500;
 
 type MetaDNetwork = 'mainnet' | 'testnet';
 
