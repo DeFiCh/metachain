3eda4fcce9e82d56c707978e0bdbc46b20a9af87
canonbrother <w.canonbrother@gmail.com>
2022-09-05 16:38:46 +0800

    test with ethers in balances.test.ts

--- ts-tests/__tests__/balances.test.ts
+++ ts-tests/__tests__/balances.test.ts
@@ -1,3 +1,4 @@
+import { ethers } from 'ethers';
 import { MetaDContainer } from '../containers';
 import {
   GENESIS_ACCOUNT,
@@ -18,24 +19,22 @@ afterAll(async () => {
 });
 
 it('should genesis balance setup correctly', async () => {
-  const bal = await container.web3.eth.getBalance(GENESIS_ACCOUNT);
-  expect(bal).toStrictEqual(GENESIS_ACCOUNT_BALANCE);
+  // web3
+  {
+    const bal = await container.web3.eth.getBalance(GENESIS_ACCOUNT);
+    expect(bal).toStrictEqual(GENESIS_ACCOUNT_BALANCE);
+  }
+
+  // ethers
+  {
+    const bal = await container.ethers.getBalance(GENESIS_ACCOUNT);
+    expect(bal.toString()).toStrictEqual(GENESIS_ACCOUNT_BALANCE);
+  }
 });
 
 it('should transfer balance', async () => {
   const value = '0x200'; // 512, must be higher than ExistentialDeposit
   const gasPrice = '0x3B9ACA00'; // 1000000000
-  const tx = await container.web3.eth.accounts.signTransaction(
-    {
-      from: GENESIS_ACCOUNT,
-      to: TEST_ACCOUNT,
-      value: value,
-      gasPrice: gasPrice,
-      gas: '0x100000'
-    },
-    GENESIS_ACCOUNT_PRIVATE_KEY
-  );
-  await container.call('eth_sendRawTransaction', [tx?.rawTransaction]);
 
   // GENESIS_ACCOUNT_BALANCE - (21000 * gasPrice) - value;
   const expectedGenesisBalance = (
@@ -43,20 +42,61 @@ it('should transfer balance', async () => {
     BigInt(21000) * BigInt(gasPrice) -
     BigInt(value)
   ).toString();
+
   const expectedTestBalance = (Number(value) - EXISTENTIAL_DEPOSIT).toString();
-  expect(
-    await container.web3.eth.getBalance(GENESIS_ACCOUNT, 'pending')
-  ).toStrictEqual(expectedGenesisBalance);
-  expect(
-    await container.web3.eth.getBalance(TEST_ACCOUNT, 'pending')
-  ).toStrictEqual(expectedTestBalance);
 
-  await container.generate();
+  // web3
+  // {
+  //   const tx = await container.web3.eth.accounts.signTransaction(
+  //     {
+  //       from: GENESIS_ACCOUNT,
+  //       to: TEST_ACCOUNT,
+  //       value: value,
+  //       gasPrice: gasPrice,
+  //       gas: '0x100000'
+  //     },
+  //     GENESIS_ACCOUNT_PRIVATE_KEY
+  //   );
 
-  expect(await container.web3.eth.getBalance(GENESIS_ACCOUNT)).toStrictEqual(
-    expectedGenesisBalance
-  );
-  expect(await container.web3.eth.getBalance(TEST_ACCOUNT)).toStrictEqual(
-    expectedTestBalance
-  );
+  //   await container.call('eth_sendRawTransaction', [tx?.rawTransaction]);
+
+  //   expect(
+  //     await container.web3.eth.getBalance(GENESIS_ACCOUNT, 'pending')
+  //   ).toStrictEqual(expectedGenesisBalance);
+  //   expect(
+  //     await container.web3.eth.getBalance(TEST_ACCOUNT, 'pending')
+  //   ).toStrictEqual(expectedTestBalance);
+
+  //   await container.generate();
+
+  //   expect(await container.web3.eth.getBalance(GENESIS_ACCOUNT)).toStrictEqual(
+  //     expectedGenesisBalance
+  //   );
+  //   expect(await container.web3.eth.getBalance(TEST_ACCOUNT)).toStrictEqual(
+  //     expectedTestBalance
+  //   );
+  // }
+
+  // ethers
+  {
+    const wallet = new ethers.Wallet(
+      GENESIS_ACCOUNT_PRIVATE_KEY,
+      container.ethers
+    );
+
+    await wallet.sendTransaction({
+      to: TEST_ACCOUNT,
+      value: value,
+      gasPrice: gasPrice,
+      gasLimit: 0x100000
+    });
+
+    await container.generate();
+
+    const gBal = await container.ethers.getBalance(GENESIS_ACCOUNT);
+    expect(gBal.toString()).toStrictEqual(expectedGenesisBalance);
+
+    const tBal = await container.ethers.getBalance(TEST_ACCOUNT);
+    expect(tBal.toString()).toStrictEqual(expectedTestBalance);
+  }
 });
