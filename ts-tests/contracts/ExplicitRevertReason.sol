5484255fff800530c442c29db5072623f980c719
canonbrother <w.canonbrother@gmail.com>
2022-09-05 16:38:22 +0800

    simplize contract

--- ts-tests/contracts/ExplicitRevertReason.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.2;
-
-contract ExplicitRevertReason {
-    function max10(uint256 a) public pure returns (uint256) {
-        if (a > 10) revert('Value must not be greater than 10.');
-        return a;
-    }
-}
--- ts-tests/contracts/Test.sol
+++ ts-tests/contracts/Test.sol
@@ -2,19 +2,32 @@
 pragma solidity ^0.8.2;
 
 contract Test {
-    function multiply(uint256 a) public pure returns (uint256 d) {
-        return a * 7;
+    string public name = 'Meta';
+
+    function mul(uint256 a, uint256 b) public pure returns (uint256) {
+        return a * b;
     }
 
-    function gasLimit() public view returns (uint256) {
-        return block.gaslimit;
+    function max10(uint256 a) public pure returns (uint256) {
+        if (a > 10) revert('Value must not be greater than 10.');
+        return a;
     }
 
-    function currentBlock() public view returns (uint256) {
+    // environmental with global vars
+    // https://docs.soliditylang.org/en/v0.8.16/units-and-global-variables.html
+    function getBlockHash(uint256 number) public view returns (bytes32) {
+        return blockhash(number);
+    }
+
+    function getCurrentBlock() public view returns (uint256) {
         return block.number;
     }
 
-    function blockHash(uint256 number) public view returns (bytes32) {
-        return blockhash(number);
+    function getGasLimit() public view returns (uint256) {
+        return block.gaslimit;
+    }
+
+    function getMsgSender() public view returns (address) {
+        return msg.sender;
     }
 }
