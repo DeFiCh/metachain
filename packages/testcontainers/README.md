78d1921d875044b8561d6797634251429643788c
Fuxing Loh <git@fuxing.dev>
2022-09-13 12:37:06 +0800

    initial commit for testcontainer refactor

---
+++ packages/testcontainers/README.md
@@ -0,0 +1,7 @@
+---
+title: Testcontainers
+---
+
+`@defimetachain/testcontainers`
+
+Provides easy to test lightweight, throwaway instances of MetaChain provisioned automatically in a Docker container.
---
+++ packages/testcontainers/src/MetaChainContainer.unit.ts
@@ -0,0 +1,28 @@
+import { MetaChainContainer } from './';
+import { CHAIN_ID } from '../utils/constant';
+
+const container = new MetaChainContainer();
+
+beforeAll(async () => {
+  await container.start();
+});
+
+afterAll(async () => {
+  await container.stop();
+});
+
+it('should have 0 hashrate', async function () {
+  expect(await container.call('eth_hashrate', [])).toStrictEqual('0x0');
+});
+
+it('should have chainId', async function () {
+  expect(Number(await container.call('net_version', []))).toStrictEqual(CHAIN_ID);
+});
+
+it('should have no account', async function () {
+  expect(await container.call('eth_accounts', [])).toStrictEqual([]);
+});
+
+it('block author should be 0x0000000000000000000000000000000000000000', async function () {
+  expect(await container.call('eth_coinbase', [])).toStrictEqual('0x0000000000000000000000000000000000000000');
+});
