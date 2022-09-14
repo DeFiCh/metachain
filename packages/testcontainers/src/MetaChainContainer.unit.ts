854703022a6a483302255aa5b24a1317daacd156
Fuxing Loh <4266087+fuxingloh@users.noreply.github.com>
2022-09-14 10:28:23 +0800

    refactor(testcontainers): move testcontainers into its own package (#67)
    #### What this PR does / why we need it:
    
    Refactor and move `ts-tests` into mono-repo style packages to allow
    separation of context and downstream usability. This is the continuation
    of #42
    
    `@defimetachain/testcontainers`
    
    Provides easy to use and test lightweight, throwaway instances of
    MetaChain provisioned automatically in a Docker
    container.
    
    `MetaChainContainer` and `StartedMetaChainContainer` follows the
    convention defined
    in
    [testcontainers/testcontainers-node](https://github.com/testcontainers/testcontainers-node)
    
    ```ts
    let container: StartedMetaChainContainer;
    let rpc: ethers.providers.JsonRpcProvider;
    
    beforeEach(async () => {
      container = await new MetaChainContainer().start();
    });
    
    afterEach(async () => {
      await container.stop();
    });
    
    it('should createBlock', async function () {
      await container.createBlock();
      expect(await rpc.getBlockNumber()).toStrictEqual(1);
    });
    ```
    
    
    #### Which issue(s) does this PR fixes?:
    <!--
    (Optional) Automatically closes linked issue when PR is merged.
    Usage: `Fixes #<issue number>`, or `Fixes (paste link of issue)`.
    -->
    Fixes #78
    
    #### Additional comments?:
    
    The current implementation in `ts-tests` isn't yet migrated to use the
    new standard and convention defined here. That should be done as part of
    https://github.com/DeFiCh/metachain/issues/79.

---
+++ packages/testcontainers/src/MetaChainContainer.unit.ts
@@ -0,0 +1,53 @@
+import { TestNet } from '@defimetachain/network';
+import { ethers } from 'ethers';
+
+import { MetaChainContainer, StartedMetaChainContainer } from '.';
+
+describe('ethers.providers.JsonRpcProvider', () => {
+  let container: StartedMetaChainContainer;
+  let rpc: ethers.providers.JsonRpcProvider;
+
+  beforeAll(async () => {
+    container = await new MetaChainContainer().start();
+    rpc = container.getEthersHttpProvider();
+  });
+
+  afterAll(async () => {
+    await container.stop();
+  });
+
+  it('should have 0 hashrate', async () => {
+    expect(await rpc.send('eth_hashrate', [])).toStrictEqual('0x0');
+  });
+
+  it('should have chainId', async () => {
+    expect(Number(await rpc.send('net_version', []))).toStrictEqual(TestNet.chainId);
+  });
+
+  it('should have no account', async () => {
+    expect(await rpc.send('eth_accounts', [])).toStrictEqual([]);
+  });
+
+  it('block author should be 0x0000000000000000000000000000000000000000', async () => {
+    expect(await rpc.send('eth_coinbase', [])).toStrictEqual('0x0000000000000000000000000000000000000000');
+  });
+});
+
+describe('utility method', () => {
+  let container: StartedMetaChainContainer;
+  let rpc: ethers.providers.JsonRpcProvider;
+
+  beforeEach(async () => {
+    container = await new MetaChainContainer().start();
+    rpc = container.getEthersHttpProvider();
+  });
+
+  afterEach(async () => {
+    await container.stop();
+  });
+
+  it('should createBlock', async () => {
+    await container.createBlock();
+    expect(await rpc.getBlockNumber()).toStrictEqual(1);
+  });
+});
