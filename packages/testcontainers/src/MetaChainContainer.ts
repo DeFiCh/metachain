0931cbec4621db9de30e7b5946cecd3a2952cfdd
Diehard073055 <eshanshafeeq073055@gmail.com>
2022-11-10 14:36:26 +0800

    update(MetachainContainer ctor)

--- packages/testcontainers/src/MetaChainContainer.ts
+++ packages/testcontainers/src/MetaChainContainer.ts
@@ -8,12 +8,12 @@ export class MetaChainContainer extends GenericContainer {
   constructor() {
     super(MetaChainContainer.image);
     this.config = TestNet;
-    this.withExposedPorts(...Object.values(this.config.ports)).withCmd(this.getCmd());
+    this.withNetworkConfig(TestNet);
   }
 
   public withNetworkConfig(config: NetworkConfig): this {
     this.config = config;
-    this.withExposedPorts(...Object.values(this.config.ports)).withCmd(this.getCmd());
+    this.withExposedPorts(...Object.values(this.config.ports)).withCommand(this.getCmd());
     return this;
   }
 
