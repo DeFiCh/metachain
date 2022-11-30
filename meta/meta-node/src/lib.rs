ad83fc23c3370e13ba3415b2a39b916ab8d39367
Diehard073055 <eshanshafeeq073055@gmail.com>
2022-11-30 18:19:46 +0800

    add empty connect_block function.

--- meta/meta-node/src/lib.rs
+++ meta/meta-node/src/lib.rs
@@ -33,10 +33,15 @@ mod ffi {
     }
 
     extern "Rust" {
+        fn connect_block(payload: DmcBlock) -> Result<(), Box<dyn Error>>;
         fn mint_block(dmc_txs: &CxxVector<DmcTx>) -> Result<DmcBlock>;
         fn parse_args_and_run(args: &CxxVector<CxxString>) -> ExecResult;
         fn interrupt_dmc() -> Result<()>;
     }
+}
+#[inline]
+fn connect_block(payload: DmcBlock) -> Result<(), Box<dyn Error>>{
+    return Ok(());
 }
 
 #[inline]
